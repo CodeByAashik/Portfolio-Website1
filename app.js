05cfda5a79dd638836e26291ced251d9d2d60da0
CodeByAashik <thakuraashik98121@gmail.com>
2025-06-04 21:24:01 +0545

    final commit

--- app.js
+++ app.js
@@ -8,3 +8,48 @@ const follower = document.querySelector('.cursor-follower');
             ease: "power2.out"
         });
     });
+
+  
+  const form = document.getElementById("contact-form");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent default redirect
+
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+      method: "POST",
+      body: formData,
+    })
+    .then(response => {
+      if (response.ok) {
+        alert("✅ Message sent successfully!");
+        form.reset(); // Clear input fields
+      } else {
+        alert("❌ Something went wrong. Please try again.");
+      }
+    })
+    .catch(error => {
+      alert("❌ Failed to send message. Check your connection.");
+      console.error("Error:", error);
+    });
+  });
+
+
+  //sidebar animation
+const sideBar = document.querySelector('.sidebar');
+const menu = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon')
+
+
+// Sidebar elements //
+menu.addEventListener("click", function(){
+    sideBar.classList.remove("close-sidebar")
+    sideBar.classList.add("open-sidebar")
+});
+
+closeIcon.addEventListener("click", function(){
+    sideBar.classList.remove("open-sidebar");
+    sideBar.classList.add("close-sidebar");
+    
+})
